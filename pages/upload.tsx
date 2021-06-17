0ff200491f7db9f13251311fc07e0253d6764881
Lachie <lachlanu@gmail.com>
2021-06-17 21:47:51 +1000

    feat: Navbar has background color when scrolled

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,37 +1,37 @@
 import { useRouter } from "next/router";
 import React from "react";
 import styled from "styled-components";
-import UploadForm from "@Components/UploadForm/UploadForm";
+import UploadForm from "@Components/UploadForm";
 import Layout from "@Components/Layout";
 import { useAuth } from "@Hooks/useAuth";
 
 const Content = styled.div`
-  margin-top: 7rem;
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+	margin-top: 7rem;
+	margin: auto;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `;
 
 const Upload = () => {
-  const router = useRouter();
+	const router = useRouter();
 
-  const { authenticated, loadingAuthState } = useAuth();
+	const { authenticated, loadingAuthState } = useAuth();
 
-  if (!authenticated && !loadingAuthState) {
-    router.push({
-      pathname: "/login",
-      query: { redirect: router.pathname }
-    });
-    return <div>Redirecting...</div>;
-  }
-  return (
-    <Layout>
-      <Content>
-        <UploadForm />
-      </Content>
-    </Layout>
-  );
+	if (!authenticated && !loadingAuthState) {
+		router.push({
+			pathname: "/login",
+			query: { redirect: router.pathname },
+		});
+		return <div>Redirecting...</div>;
+	}
+	return (
+		<Layout>
+			<Content>
+				<UploadForm />
+			</Content>
+		</Layout>
+	);
 };
 
 export default Upload;
