0eb04f2cecbea3ca9436cd46624fac71f800a97f
Lachie <lachlanu@gmail.com>
2021-06-21 10:07:14 +1000

    chore: Tidied up button component and fixed breakpoints

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,99 +2,83 @@
 import React, { FunctionComponent } from "react";
 import styled from "styled-components";
 
-interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
-  onClick?: () => void;
-  disabled?: boolean;
-  type?: "button" | "submit" | "reset";
-  primary?: boolean;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	onClick?: () => void;
+	disabled?: boolean;
+	type?: "button" | "submit" | "reset";
+	primary?: boolean;
 }
 
-const defaultProps: ButtonProps = {
-  onClick: () => {},
-  disabled: false
-};
+const StyledButton = styled.button<ButtonProps>`
+	font-family: "Inter";
+	font-size: 1rem;
+	height: 2.5em;
+	background: ${(props) =>
+		props.primary ? props.theme.colors.accentGradient : "white"};
+	border: 1px solid #dbdbdb;
+	border-radius: 4px;
+	color: ${(props) =>
+		props.primary ? props.theme.colors.fg : props.theme.colors.fg};
+	cursor: pointer;
+	padding-bottom: calc(0.5em - 1px);
+	padding-left: 1em;
+	padding-right: 1em;
+	padding-top: calc(0.5em - 1px);
+	display: inline-block;
 
-const BlandButton: FunctionComponent<ButtonProps> = ({
-  children,
-  onClick,
-  disabled,
-  type,
-  ...rest
-}: ButtonProps) => {
-  const handleClick = (e: React.MouseEvent) => {
-    e.stopPropagation();
+	transition: all 0.05s ease;
 
-    if (!disabled && onClick) {
-      onClick();
+	&:hover {
+		background: ${({ primary, theme }) =>
+			primary ? theme.colors.transparentAccent : "#f0f0f0"};
+		border-color: #cbcbcb;
+	}
+
+	${({ primary, theme }) =>
+		primary &&
+		`
+    border-color: transparent;
+
+    &:hover {
+      background-color: ${theme.colors.transparentAccent};
     }
-  };
+  `}
 
-  return (
-    <button
-      onClick={handleClick}
-      disabled={!!disabled}
-      type={type || "button"}
-      {...rest}
-    >
-      {children}
-    </button>
-  );
-};
-
-BlandButton.defaultProps = defaultProps;
-
-const Button = styled<FunctionComponent<ButtonProps>>(
-  ({ primary, ...rest }) => <BlandButton {...rest} />
-)`
-  font-family: "Inter";
-  font-size: 1rem;
-  height: 2.5em;
-  background-color: white;
-  border: 1px solid #dbdbdb;
-  border-radius: 4px;
-  color: #363636;
-  cursor: pointer;
-  padding-bottom: calc(0.5em - 1px);
-  padding-left: 1em;
-  padding-right: 1em;
-  padding-top: calc(0.5em - 1px);
-  display: inline-block;
-
-  transition: all 0.05s ease;
-
-  &:hover {
-    background-color: #f0f0f0;
-    border-color: #cbcbcb;
-  }
-
-  ${(props) => {
-    if (props.primary) {
-      return `
-				background-color: #00d1b2;
-				border-color: transparent;
-				color: #fff;
-
-				&:hover {
-					background-color: #00c4a7;
-				}
-			`;
+	${({ disabled }) =>
+		disabled &&
+		`
+    background-color: grey;
+    &:hover {
+      background-color: grey;
     }
-
-    return null;
-  }}
-
-  ${(props) => {
-    if (props.disabled) {
-      return `
-				background-color: grey;
-				&:hover {
-					background-color: grey;
-				}
-			`;
-    }
-
-    return null;
-  }}
+  `}
 `;
+
+const Button: FunctionComponent<ButtonProps> = ({
+	children,
+	onClick,
+	disabled,
+	type,
+	...rest
+}: ButtonProps) => {
+	const handleClick = (e?: React.MouseEvent<HTMLButtonElement>) => {
+		if (e) e.stopPropagation();
+
+		if (!disabled && onClick) {
+			onClick();
+		}
+	};
+
+	return (
+		<StyledButton
+			onClick={handleClick}
+			disabled={!!disabled}
+			type={type || "button"}
+			{...rest}
+		>
+			{children}
+		</StyledButton>
+	);
+};
 
 export default Button;
